4a1ea02b18766cc3d136a8c26a578e0b614f5bd1
zhangqin <qinzhang@didiglobal.com>
2022-11-14 19:49:58 +0800

    复习螺旋矩阵

--- 算法/1107-1111/solutions/53. maxSubArray.js
+++ 算法/1107-1111/solutions/53. maxSubArray.js
@@ -3,5 +3,25 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
+  if (nums.length === 1) {
+    return nums;
+  }
+  let max = Number.MIN_SAFE_INTEGER;
+  let temp = 0;
+  let i = 0, j = 0;
+  while(i <= j && j < nums.length) {
+    if (temp + nums[j] >= temp) {
+      temp += nums[j];
+      j ++;
+      continue;
+    }
+    if (temp > max) {
+      max = temp;
+    }
+    i ++;
+    j = i;
+  }
+  return max;
 };
+
+console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
--- 算法/1107-1111/solutions/54. spiralOrder.js
+++ 算法/1107-1111/solutions/54. spiralOrder.js
@@ -2,6 +2,39 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
- var spiralOrder = function(matrix) {
+var spiralOrder = function(matrix) {
+  if (!matrix.length || !matrix[0].length) {
+    return [];
+  }
 
+  const result = [];
+  let top = 0,
+      right = matrix[0].length - 1,
+      bottom = matrix.length - 1,
+      left = 0;
+
+  while(top <= bottom && left <= right) {
+    for (let i = left; i <= right; i ++) {
+      result.push(matrix[top][i]);
+    }
+    for (let i = top + 1; i <= bottom; i ++) {
+      result.push(matrix[i][right]);
+    }
+    if (top < bottom && left < right) {
+      for(let i = right - 1; i >= left; i --) {
+        result.push(matrix[bottom][i]);
+      }
+      for (let i = bottom - 1; i > top; i --) {
+        result.push(matrix[i][left]);
+      }
+    }
+    top = top + 1;
+    right = right - 1;
+    bottom = bottom - 1;
+    left = left + 1;
+  }
+
+  return result;
 };
+
+console.log(spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]));
