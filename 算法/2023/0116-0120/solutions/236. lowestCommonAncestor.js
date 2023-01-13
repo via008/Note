70c6acbc11dc6f19e2f912d927f6348f9f5ec844
zhangqin <qinzhang@didiglobal.com>
2023-01-13 11:02:22 +0800

    二叉树的公共祖先

--- 算法/2023/0116-0120/solutions/236. lowestCommonAncestor.js
+++ 算法/2023/0116-0120/solutions/236. lowestCommonAncestor.js
@@ -11,6 +11,32 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
- var lowestCommonAncestor = function(root, p, q) {
-    
+var lowestCommonAncestor = function(root, p, q) {
+    // 拿到一个节点的所有父级
+    const getParents = (root, node) => {
+      if (root !== null) {
+        if (root === node) {
+          return [node];
+        }
+        const l = getParents(root.left, node);
+        const r = getParents(root.right, node);
+        if (l) {
+          return [...l].concat(root);
+        }
+        if (r) {
+          return [...r].concat(root);
+        }
+      }
+    }
+
+    const pParents = getParents(root, p);
+    const qParents = getParents(root, q);
+
+    for(let i = 0; i < pParents?.length; i ++) {
+      for(let j = 0; j < qParents?.length; j ++) {
+        if (pParents[i] === qParents[j]) {
+          return pParents[i];
+        }
+      }
+    }
 };
